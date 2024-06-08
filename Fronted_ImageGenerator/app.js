a1b516eb5e5a026795655ae76bf23123fbf3a05a
ANKIT MISHRA <148037556+ankit938@users.noreply.github.com>
2024-06-08 21:50:52 +0530

    Add files via upload

---
+++ Fronted_ImageGenerator/app.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from './src/pages/common/Navbar/navbar.js';
+import HomePage from "./src/pages/HomePage/homepage.js";
+import ImageGenerator from './src/pages/ImageGenerator/imageGenerator.js';
+import HistoryPage from "./src/pages/HistoryPage/historyPage.js";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/image-generator",
+        element: <ImageGenerator />,
+    },
+    {
+        path: '/history',
+        element: <HistoryPage />
+    }
+]);
+
+const parent = document.getElementById("root");
+const root = ReactDOM.createRoot(parent);   
+
+const App = ()=>{
+    return <RouterProvider router={router} />;
+}
+
+root.render(<App />);
